430545dd2fc4c172c84bace6f00a9c01725b0eba
Rathi <github email address>
2017-05-30 12:06:45 -0400

    did this lab for understang object

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var recipes = { }
+
 function updateObjectWithKeyAndValue(obj, key, value){
 
 return Object.assign({}, obj, { [key]: value })
+
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
